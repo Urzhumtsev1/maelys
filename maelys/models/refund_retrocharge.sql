5b20bde4274df62f83bd07796d314e76afa838a5
Maxim Urzhumtsev <urzhumtsev.max@gmail.com>
2022-10-13 15:42:21 +0300

    deleted comments

--- maelys/models/refund_retrocharge.sql
+++ maelys/models/refund_retrocharge.sql
@@ -5,8 +5,7 @@ with source_data as (
    select * from {{ ref('parsed_data_types') }}
 
 )
--- too much to do
--- checkpoint: first create stage tables for Marketplace, Payment
+
 select 
     order_id,
     "_id" as marketplace_id,

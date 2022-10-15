9d64b7dbc1431895011ef1f08a11ac605859ac26
Maxim Urzhumtsev <urzhumtsev.max@gmail.com>
2022-10-15 15:29:41 +0300

    fix payment_id

--- maelys/models/core/refund_retrocharge.sql
+++ maelys/models/core/refund_retrocharge.sql
@@ -19,7 +19,7 @@ marketplace_pre_selection as (
 select 
     s.order_id,
     m.marketplace_id,
-    1 as payment_id
+    s.id as payment_id
 from source_data as s
 inner join marketplace_pre_selection as m on m.id = s.id
 
